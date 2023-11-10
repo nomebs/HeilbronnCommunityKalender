31c80bd56a684ff4525d01d14015098a07427813
Joshua Franz <76621974+Joshua-Franz-Vollkornbyte@users.noreply.github.com>
2023-11-11 00:27:08 +0100

    Navbar
    React App initalisiert und erste Navbar links eingefügt

---
+++ community-kalender/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import LeftNavbar from "./components/LeftNavbar.js";
+import'./index.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <LeftNavbar />
+     <div className="main-content">
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ community-kalender/src/components/LeftNavbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class LeftNavbar extends React.Component{
+    render(){
+        return(
+            <div className="left-navbar separator">
+                <u1>
+                    <li><a href="#">Site 1</a></li>
+                    <li><a href="#">Site 2</a></li>
+                    <li><a href="#">Site 3</a></li>
+                </u1>
+            </div>
+        );
+    }
+}
+
+export default LeftNavbar;
